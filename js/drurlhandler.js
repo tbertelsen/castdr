f0fc3793c720f72157ede5a1f51dccb50a36b348
Tobias Bertelsen <tobias.bertelsen@gmail.com>
2014-09-14 16:18:43 +0200

    Merged with URL Player
    Copying and modding Chromecast URL player.

---
+++ js/drurlhandler.js
@@ -0,0 +1,125 @@
+function DrUrlHandler () {
+    this.mediaUrl = "";
+    this.title = "";
+    this.imgUrl = ""
+}
+ 
+DrUrlHandler.prototype.getInfo = function() {
+    return this.color + ' ' + this.type + ' apple';
+};
+
+DrUrlHandler.prototype.handleUrl = function(url) {
+	try {
+		var slugs = interpretDrUrl(url);
+		this.fetchProgramCard(slugs.episodeSlug);
+	} catch (e) {
+		console.log(e)
+		alert("Ooops!\n" + e)
+		this.onError(e)
+	}
+}
+
+DrUrlHandler.prototype.fetchProgramCard = function(slug) {
+	var url = "http://www.dr.dk/muTest/api/1.1/programcard/" + slug;
+	return fetchJson(url, this.fetchManifest.bind(this));
+}
+
+DrUrlHandler.prototype.fetchManifest = function(programCard) {
+	console.log("programCard")
+	console.log(programCard)
+	asset = programCard.PrimaryAsset;
+	console.log("asset")
+	console.log(asset)
+	if (asset.Kind != "VideoResource") {
+		throw "Not a video link";
+	}
+	if (asset.RestrictedToDenmark) {
+		console.log("Denmark only program! Unkown international behaviour.")
+	}
+	this.title = programCard.Title;
+	this.imgUrl = programCard.PrimaryImageUri;
+
+	var url = asset.Uri;
+	return fetchJson(url, this.selectVideoUrl.bind(this));
+}
+
+DrUrlHandler.prototype.selectVideoUrl = function(manifest) {
+	console.log("manifest")
+	console.log(manifest)
+	for (i in manifest.Links) {
+		link = manifest.Links[i]
+		if (link.Target == "HLS") {
+			this.mediaUrl = link.Uri;
+			this.success();
+			return;
+		}
+	}
+}
+
+DrUrlHandler.prototype.success = function() {
+	this.onSuccess(this.mediaUrl, this.title, this.imgUrl);
+}
+
+/**
+ * Callback for clients.
+ */
+DrUrlHandler.prototype.onSuccess = function(mediaUrl, title, imgUrl) {
+	console.log("Successfully fetched DR URL:")
+	console.log(this)
+	console.log(mediaUrl)
+	console.log(title)
+	console.log(imgUrl)
+}
+
+/**
+ * Callback for clients.
+ */
+DrUrlHandler.prototype.onError = function(errorMsg) {
+	
+}
+
+
+function xhrSuccess () { this.callback(JSON.parse(this.responseText)); }
+
+function xhrError () { console.error(this.statusText); }
+
+function fetchJson (sURL, fCallback) {
+	console.log("Getting " + sURL)
+  var oReq = new XMLHttpRequest();
+  oReq.callback = fCallback;
+  oReq.arguments = Array.prototype.slice.call(arguments, 2);
+  oReq.onload = xhrSuccess;
+  oReq.onerror = xhrError;
+  oReq.open("get", sURL, true);
+  oReq.send(null);
+}
+
+function interpretDrUrl(url) {
+	var index = url.search(/dr.dk\//i)
+	if (index == -1) {
+		throw  "Not a dr.dk url";
+	}
+	var pathStart = index + "dr.dk/".length;
+	var path = url.substring(pathStart).split(/[\/#?~!*()';]/);
+	
+  if (path.length < 4) {
+  	throw "URL is not long enough"
+  }
+
+	if (!("tv" == path[0] && "se" == path[1])) {
+		throw "URL path must start with /tv/se";
+  }
+
+  // Ensure no bad characters in ids
+  if (!(encodeURIComponent(path[2]) == path[2] &&
+  	encodeURIComponent(path[3]) == path[3])) {
+  	throw "Bad characters in URL"
+  }
+
+  return {	
+	  seriesSlug : path[2],
+	  episodeSlug : path[3]
+  };
+}
+
+
---
+++ js/urlplayer.js
@@ -0,0 +1,136 @@
+
+var autoLaunch = false;
+
+function onload() {
+  var url = getUrlParameter("url")
+  if (url) {
+    $('#url').val(url);
+    autoLaunch = true;
+  }
+}
+
+function getUrlParameter(sParam) {
+    var sPageURL = window.location.search.substring(1);
+    var sURLVariables = sPageURL.split('&');
+    for (var i = 0; i < sURLVariables.length; i++)  {
+        var sParameterName = sURLVariables[i].split('=');
+        if (sParameterName[0] == sParam) {
+            return decodeURIComponent(sParameterName[1]);
+        }
+    }
+}
+
+var player;
+var drUrlHandler;
+
+$(function() {
+  player = new CastPlayer();
+  drUrlHandler = new DrUrlHandler();
+  drUrlHandler.onSuccess = _startPlayback;
+  drUrlHandler.onError = showTryAgain;
+});
+
+function showTryAgain() {
+  $('#player_now_playing').html('Please try agian.');
+  $('#loading_img').hide();
+}
+
+function launchApp() {
+  player.launchApp();
+}
+
+function getContentType(url) {
+  var ext = url.split('.').pop();
+  var formats = [
+    {ext: 'mkv', type: 'video'},
+    {ext: 'webm', type: 'video'},
+    {ext: 'mp4', type: 'video'},
+    {ext: 'm4v', type: 'video'},
+    {ext: 'm4a', type: 'audio'},
+    {ext: 'jpeg', type: 'image'},
+    {ext: 'jpg', type: 'image'},
+    {ext: 'gif', type: 'image'},
+    {ext: 'png', type: 'image'},
+    {ext: 'bmp', type: 'image'},
+    {ext: 'webp', type: 'image'}
+  ];
+  for (var i = 0; i < formats.length; i++) {
+    if (formats[i].ext == ext) {
+      return formats[i].type + "/" + ext;
+    }
+  }
+  // it doesn't matter now, as it's unsupported.
+  return "";
+}
+
+function startPlayback() {
+  if (player.session == null || $('#url').val().trim() == "") {
+    console.log("Skipping playback")
+    return;
+  }
+  var url = decodeURIComponent($('#url').val());
+  $('#player_now_playing').html('Please wait.');
+  $('#loading_img').show();
+  $('#controls').show();
+  $('#loading_img').show();
+  drUrlHandler.handleUrl(url);
+}
+
+function _startPlayback(mediaUrl, title, imgUrl) {
+    var contentType = getContentType(mediaUrl);
+    player.loadMedia(mediaUrl, contentType, title, imgUrl);
+    $('#player_now_playing').html('Now playing ' + title);
+    $('#player_img').attr("src", imgUrl).show();
+    $('#loading_img').hide();
+    $('#controls').show();
+  }
+
+function pause() {
+  if (player.session != null) {
+    player.pauseMedia();
+  }
+}
+
+function resume() {
+  if (player.session != null) {
+    player.playMedia();
+  }
+}
+
+function seek(is_forward) {
+  if (player.session != null) {
+    player.seekMedia(parseInt($("#player_seek").val()), is_forward);
+  }
+}
+
+function seekTo() {	
+  if (player.session != null) {
+    player.seekTo(parseInt($("#player_seek_range").val()));
+  }
+}
+
+function stop() {
+  var reply = confirm("This will stop playback on the TV. Are you sure?");
+  if (reply == true) {
+    player.stopApp();
+    $('#controls').hide();
+  }
+}
+
+function volumeDown() {
+  if (player.session != null) {
+      player.volumeControl(false, false);
+  }
+}
+
+function volumeUp() {
+  if (player.session != null) {
+    player.volumeControl(true, false);
+  }
+}
+
+function volumeMute() {
+  if (player.session != null) {
+    player.volumeControl(false, true);
+  }
+}
